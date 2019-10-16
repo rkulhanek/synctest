604cde770ee8803e38b35e3d4c0856fb038fe8af
Ray Kulhanek <kulhanek.5@wright.edu>
2019-10-16 15:42:47 -0400

    synchrepos makes it through stresstest. still need to compare resulting repos

---
+++ stresstest.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+TESTDIR="$HOME/code/QA/synctest/stresstest"
+
+if [ -e "$TESTDIR" ]; then
+	echo "Directory '$TESTDIR' already exists. Delete it or change \$TESTDIR before starting this." > /dev/stderr
+	exit 1
+fi
+
+mkdir -p "$TESTDIR"
+cd "$TESTDIR"
+
+if [ -e local -o -e remote0 -o -e remote1 ]; then
+	exit 1
+fi
+
+# Create repos.
+mkdir local
+mkdir remote0
+mkdir remote1
+cd "$TESTDIR/remote0"
+git init --bare
+cd "$TESTDIR/remote1"
+git init --bare
+
+# create common starting point
+cd "$TESTDIR/local"
+git init
+echo 'original foo' > foo
+git add foo
+git commit -m 'init'
+git branch A
+git commit -m 'add branch A'
+git remote add remote0 "$TESTDIR/remote0"
+git remote add remote1 "$TESTDIR/remote1"
+git push --all remote0
+git push --all remote1
+
+# Other users show up and push changes to remote repos without synchronizing
+cd "$TESTDIR"
+git clone remote0 user0
+git clone remote1 user1
+
+cd "$TESTDIR/user0"
+git branch B
+echo 'user0 foo' > foo
+git add foo
+git commit -m 'user0 modified foo'
+git checkout A
+echo 'user0 Abar' > Abar
+git add Abar
+git commit -m 'user0 added Abar'
+git checkout B
+echo 'user0 Bfoo' > Bfoo
+echo 'user0 Bbaz' > Bbaz
+git add Bfoo Bbaz
+git commit -m 'user0 added files to branch B'
+git push --all
+git checkout master
+
+cd "$TESTDIR/user1"
+git branch '$Crazy"name' # n.b. git won't allow branches with : in the name, so we don't need to test for that.
+echo 'user1 foo' > foo
+git add foo
+git commit -m 'user1 modified foo'
+git checkout '$Crazy"name'
+echo 'Cfoo' > Cfoo
+git add Cfoo
+git commit -m 'user1 added Cfoo'
+git push --all
+git checkout master
+
+# And now we make our own incompatible changes
+cd "$TESTDIR/local"
+git branch B
+echo 'local foo' > foo
+git add foo
+git commit -m 'local modified foo (all three repos now have incompatible copies of foo)'
+git checkout A
+echo 'local Afoo' > Afoo
+git add Afoo
+git commit -m 'local added Afoo'
+git checkout B
+echo 'local Bfoo' > Bfoo
+echo 'local Bbar' > Bbar
+git add Bfoo Bbar
+git commit -m 'local added Bfoo (incompatible with remote0 version), Bbar'
+# explicitly staying out of master branch. make sure it doesn't try to check out master into B
+
+# TODO: run synctest here
+# TODO: checkout a new copy of each of remote0, remote1, and verify that each is identical to local
+
--- syncrepos.sh
+++ syncrepos.sh
@@ -1,8 +1,77 @@
-#!/bin/bash
+#!/bin/bash -x
 
-repos=(github gitlab)
+#remotes=(github gitlab)
+remotes=(remote0 remote1)
 
-for r in ${repos[@]}; do
-	git push --all "$r"
+# TODO: Set a working directory other than the current one to do this in. If things
+# get weird, I want it clobbering a directory I don't care about
+
+DEFAULT_COLOR=0
+RED=31
+GREEN=32
+YELLOW=33
+BLUE=34
+BRIGHT_RED=91
+BRIGHT_GREEN=92
+BRIGHT_YELLOW=93
+BRIGHT_BLUE=94
+
+function color {
+	if [ "$DEFAULT_COLOR" -eq "$1" ]; then
+		echo "$2"
+		return
+	fi
+	echo -e "\x1b[$1m$2\x1b[0m";
+}
+
+function error_msg {
+	color "$BRIGHT_RED" "ERROR: $1"
+}
+
+# Verify no uncommited local changes
+git diff-index --quiet HEAD
+if [ 0 -ne "$?" ]; then
+	error_msg 'There exist uncommitted changes. Commit them before synchronizing remote repos.'
+	exit 1
+fi
+
+git branch -r
+for remote in ${remotes[@]}; do
+	AWK_FILTER="/^$remote/ { print(substr(\$0, 2+length(\"$remote\"))) }"
+	git fetch -- "$remote" # needed to make new branches show up in git branch -r
+
+	remote_branches=$(git branch -r | grep -v ' -> ' | sed 's/^ *//' | awk "$AWK_FILTER") # skip, e.g. HEAD -> origin/HEAD
+	color $BRIGHT_BLUE "remote: $remote"
+	for branch in $remote_branches; do
+		color $BRIGHT_BLUE "branch: $remote/$branch"
+		git checkout "$branch"
+		git pull -- "$remote" "$branch"
+		git mergetool
+		git commit -m "merged $remote/$branch"
+	done
 done
+
+exit 0
+
+# TODO: Actually, the push should be a separate script.
+
+# TODO: add a -y option that bypasses this
+while true; do
+	read -p "$(color $DEFAULT_COLOR 'Push changes (yes/no)? ')" yn
+	if [ 'yes' == "$yn" ]; then
+		break
+	elif [ 'no' == "$yn" ]; then
+		echo 'Aborting'
+		exit 1
+	fi
+done
+
+color $RED 'TODO: remote below exit'
+exit 0
+
+
+# Done merging
+#for remote in ${remotes[@]}; do
+#	git push --all "$remote"
+#done
 
